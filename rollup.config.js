22768c93d89da8f87e1e8419c6246e50799c4fb6
Rinat <rinat@Rinats-MacBook-Air.local>
2025-06-02 16:11:32 +0900

    added rullup

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'es',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json'
+    })
+  ],
+  external: ['@hicaru/chacharand.js']
+};
